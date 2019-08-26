bedc15c356c9f9a78352b9226c735519c3a6c193
Rahuldeveloper62 <54538591+Rahuldeveloper62@users.noreply.github.com>
2019-08-26 18:20:12 +0530

    change1
    test

---
+++ test.java
@@ -0,0 +1,14 @@
+class test {
+
+  
+	public static void main(String [] args){
+        
+       
+
+     int [][]a={{1,2,3},{4,5,6}};
+	 System.out.println(a.length);
+      System.out.print(a[1][2]);		
+		
+	}
+	
+} 
